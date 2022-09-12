e93b66d195021348be0246b6fa84edd75e2c0dde
cuongnqph06515 <cuongnqph06515@gmail.com>
2022-09-12 16:21:34 +0700

    commit source code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './user-pages/home/home.component';
+import { ContactComponent } from './user-pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
